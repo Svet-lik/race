3b4dee4d7773f5ab19a62d24d3463e11441cc440
Svet-lik <svet-st@yandex.ru>
2020-09-09 00:52:53 +0300

    Day1 - correct

--- main.js
+++ main.js
@@ -3,23 +3,18 @@ const score = document.querySelector('.score'),
     gameArea = document.querySelector('.game-area'),
     car = document.createElement('div');
 
-car.classList.add('car');
-start.addEventListener('click', startGame);
-document.addEventListener('keydown', startRun);
-document.addEventListener('keyup', stopRun);
-
 const keys = {
     ArrowUp: false,
     ArrowDown: false,
     ArrowRight: false,
     ArrowLeft: false
-}
+};
 
 const setting = {
     start: false,
     score: 0,
     speed: 3
-}
+};
 
 function startGame() {
     start.classList.add('hide');
@@ -29,17 +24,25 @@ function startGame() {
 }
 
 function playGame() {
+    console.log('Play game!');
     if (setting.start) {
         requestAnimationFrame(playGame);
     }
 }
 
 function startRun(event) {
-    event.preventDefault();
-    keys[event.key] = true;
+    if (event.key !== 'F5' && event.key !=='F12') {
+        event.preventDefault();
+        keys[event.key] = true;
+    }
 }
 
 function stopRun(event) {
     event.preventDefault();
     keys[event.key] = false;
 }
+
+car.classList.add('car');
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
